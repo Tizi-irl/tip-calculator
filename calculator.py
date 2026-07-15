53af17b85932ead0bd0c0673b61511f11d964b53
Tizi-irl <FischerTizian@outlook.de>
2026-07-15 19:19:16 +0200

    Calculator.exe 1.0.1
    -minor bug fixes, the application wont close automaticly now, unless the user is typing anything into the console

---
+++ calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+bill = float(input("How much was the total bill?"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people split the bill?"))
+bill_with_tip = tip / 100 * bill + bill
+print(bill_with_tip)
+input("\nPress Enter to exit...")
